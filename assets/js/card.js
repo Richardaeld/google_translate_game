fc358ddbe5d7de23dba79342e199b6c2d10b3694
Richardaeld <richardaeld@gmail.com>
2020-08-25 02:20:00 +0000

    Added an auto fill text for cards and an auto class assign through card.js, created an objectAndArray.js for housing those and restructed the index.html with more cards and started to prep for a more minimal code structure in index.html

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,17 +1,73 @@
-let words = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"];
 
 
-$(document).ready(function() {
+$(document).ready(function() {  
+    var times;
+    var cardLabel = 0;
+
+    times = $(".card-frame").length;        //counts amount of card-frames for numbering
     
-});
+    $(".card-frame").first().children().children().addClass("card-text-" + cardLabel)       //labels start div card-text-* 
 
-$(document).ready(function(){
-    var content;
-    for (content=0; content < 10; content++) {
-        console.log("hey listen" + content);
-        $(".card-text-" + content).text(words[content]);
+    for (cardLabel = 1; cardLabel < times; cardLabel++ ) {   //labels divs for remaining card-text-*
+        $("div[class*='card-text-']").last().parent().parent().next("div").children().children().addClass("card-text-" + cardLabel);              
     };
 
-    $(".card-frame").children().children().addClass("card-word");
-    
 });
+
+$(".card-frame").click(function() {
+    $(this).slideToggle("slow", function() {
+        $(this).children().children().text("boologa");
+    });
+    $(this).slideToggle();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$(document).ready(function(){
+//    var content;
+//    for (content=0; content < words.length; content++) {
+//      //  console.log("hey listen" + content);
+ //       $(".card-text-" + content).text(words[content]);
+//    };
+//    $(".card-frame").children().children().addClass("card-word");
+//});
+
+//$(document).ready(function() {
+//    var cardContent =0;
+
+//    for (cardContent = 0; cardContent < words.length; cardContent++ ) {
+ //       $(".card-frame").children().children().first().text(words[cardContent]);
+  //      for (cardContent = 1; cardContent < words.length; cardContent++ ) {
+//        $(".card-frame").children().children().first().sibling().text(words[cardContent]);
+
+//        };
+//    };
+
+//});
+
+//$(".card-frame").click(function() { //hard setting with dissappearing cards
+
+//    $(this).toggle("slow");
+//    $(this).toggle("slow").text("Im different");
+
+//    $(this).animate({height: "200px"}, 1000);
+
+//    $(this).css("transition-duration", "2s");
+//    $(this).css("transform", "rotateY(360deg)");
+//});
---
+++ assets/js/objectsAndArrays.js
@@ -0,0 +1,13 @@
+let words = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"];
+
+var cardEasy = {
+    height: "7rem",
+};
+
+var cardMedium = {
+
+};
+
+var cardDifficult = {
+
+};
