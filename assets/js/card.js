c401fbd7b08a26b0b5e2f34710282db25ba6be5f
Richardaeld <richardaeld@gmail.com>
2020-08-26 01:09:29 +0000

    card.js adds duplicates of every card for a matching pair

--- assets/js/card.js
+++ assets/js/card.js
@@ -6,56 +6,47 @@ $(document).ready(function() {  //start ready
 
     cardCount = $(".card-frame").length;                                                                                //counts amount of card-frames for numbering
     
-    $(".card-frame").odd().addClass("card-img-odd");                                                                    //adds background to odd cards
-    $(".card-frame").even().addClass("card-img-even");                                                                  //adds background to even cards
     $(".card-frame").first().children().children().addClass("card-text-" + cardUniqueLabel).addClass("card-word").addClass("invisible").text(words[cardUniqueLabel]);       //labels start div card-text-* 
     $(".card-frame").first().addClass("ml-3");                                                                          //labels start div for space (margin)
 
     for (cardUniqueLabel = 1; cardUniqueLabel < cardCount; cardUniqueLabel++ ) {                                         //labels divs for remaining card-text-*
         $("div[class*='card-text-']").last().parent().parent().next("div").children().children().addClass("card-text-" + cardUniqueLabel).addClass("card-word").addClass("invisible").text(words[cardUniqueLabel]);  //adds card specific targets and text spacing
         $("div[class*='card-text-']").last().parent().parent().addClass("ml-3");                                        //spaces (margins) cards
-    };
-
-    for (cardUniqueLabel = 0; cardUniqueLabel < cardCount; cardUniqueLabel++ ) {
-        $(".card-text-")
     }
 
-});                             //end start ready
+     for (cardUniqueLabel = cardCount; cardUniqueLabel < cardCount + cardCount; cardUniqueLabel++ ) {                                       //creates pair for matching
+        $("#game-board").append("<div class='col-2 card-frame'><div class='row no-gutters'><div class='col-12'><span></span></div></div></div>");
+        $("div[class*='card-text-']").last().parent().parent().next("div").children().children().addClass("card-text-" + (cardUniqueLabel - cardCount)).addClass("card-word").addClass("invisible").text(words[cardUniqueLabel - cardCount]);  //adds card specific targets and text spacing
+        $("div[class*='card-text-']").last().parent().parent().addClass("ml-3");                                        //spaces (margins) cards
+     }
+    
+    $(".card-frame").odd().addClass("card-img-odd");                                                                    //adds background to odd cards
+    $(".card-frame").even().addClass("card-img-even");                                                                  //adds background to even cards
 
 
 
-$(".card-frame").click(function() {  // flips card and hides/reveals text
-    $(this).slideToggle("slow", function() {
+    $(".card-frame").click(function() {  // flips card and hides/reveals text
+        $(this).slideToggle("slow", function() {
         
-        if ($(this).hasClass("card-img-even")) {        //flip illusion to even cards
-            $(this).addClass("even").removeClass("card-img-even");
-            $(this).children().children().removeClass("invisible");
-        } else if ($(this).hasClass("even")) {
-            $(this).addClass("card-img-even").removeClass("even");
-            $(this).children().children().addClass("invisible");  
-        };
+            if ($(this).hasClass("card-img-even")) {        //flip illusion to even cards
+                $(this).addClass("even").removeClass("card-img-even");
+                $(this).children().children().removeClass("invisible");
+            } else if ($(this).hasClass("even")) {
+                $(this).addClass("card-img-even").removeClass("even");
+                $(this).children().children().addClass("invisible");  
+            }
         
-        if ($(this).hasClass("card-img-odd")) {         //flip illusion to odd cards
-            $(this).addClass("odd").removeClass("card-img-odd");
-            $(this).children().children().removeClass("invisible")
-        } else if ($(this).hasClass("odd")) {
-            $(this).addClass("card-img-odd").removeClass("odd");
-            $(this).children().children().addClass("invisible");  
-        };
-        
+            if ($(this).hasClass("card-img-odd")) {         //flip illusion to odd cards
+                $(this).addClass("odd").removeClass("card-img-odd");
+                $(this).children().children().removeClass("invisible");
+            } else if ($(this).hasClass("odd")) {
+                $(this).addClass("card-img-odd").removeClass("odd");
+                $(this).children().children().addClass("invisible");  
+            }
+        $(this).slideToggle();
     });
 
-    $(this).slideToggle();
 });
 
-
-
-
-
-
-
-
-
-
-
+});                             //end start ready DONT FORGET Event Delegation
 
