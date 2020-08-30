e8b2b0aa22b4139ff1c51955e34743b33db2946e
Richardaeld <richardaeld@gmail.com>
2020-08-30 02:21:21 +0000

    Beautified code

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,4 +1,3 @@
- 
  //--------------  global variables
     var maxPoints = 0;
     var playerPoints = 0;
@@ -8,30 +7,12 @@
     var chosenId1 = null;       // for each card index 
     var chosenId2 = null;
     var cardCount = null;
-    var playerModeSelection= [];
+    var playerModeSelection= [];  //Must be present to boot and logs all user selections
 //---------------- end global variables
 
 
 $(document).ready(function() {  //start creation ready for card population and background adding
-
     startScreen();              //creats the start screen
-
-$(".start-button").click(function() {  
-
-        // {[0]mode, [1]difficulty, [2]#cards, [3]language, [4]time }
-        $(".start-screen option:selected").each(function() {
-            playerModeSelection.push($(this).index());
-            $(".start-button").append(playerModeSelection);
-        });
-
-            cardPopulate();
-            $(".start-screen").css("display", "none");
-            $(".start-screen").css("z-index", "0");
-            console.log(playerModeSelection);
-        });
-
-        playGame();
-
 });                             //end creation ready DONT FORGET Event Delegation
 
 
@@ -42,58 +23,6 @@ $(".start-button").click(function() {
 
 
 
-function playGame() {
 
-    $(document).on("click", ".card-frame", function() {
-        if(gameMatchingPair === 0 && !$(this).hasClass("card-matched")) {
-            chosenIndex1 = $(this).index(); // catches index where click is
-            chosenId1 = chosenIndex1;       
-            gameMatchingPair++;
-        } else if (gameMatchingPair === 1 && chosenId1 !== $(this).index() && !$(this).hasClass("card-matched")) { // prevents same selections
-            chosenIndex2 = $(this).index();
-            chosenId2 = chosenIndex2;
-            gameMatchingPair++;
-        }
-
-        console.log("click"); // Error testing and console readability  
-
-        if ($(this).hasClass("card-img-even") && (!$(this).hasClass("clicked"))) {
-            flipEven ($(this).index());
-        } else if ($(this).hasClass("card-img-odd") && !$(this).hasClass("clicked")) {
-            flipOdd ($(this).index());
-        }
-
-        if (gameMatchingPair === 2) {
-            if(chosenIndex1 >= cardCount){     //makes chosenIndex's comparable for matching
-                chosenIndex1 = chosenIndex1 - cardCount;
-            } else if (chosenIndex2 >= cardCount) {
-                chosenIndex2 = chosenIndex2 -cardCount;
-            }
-
-            if(chosenIndex1 === chosenIndex2) {     //removes correct cards and reflips wrong
-                timeDelay(chosenId1);
-                timeDelay(chosenId2);
-                playerScored();
-            } else {
-
-                    if (chosenId1 % 2 === 0) {
-                        coverEven(chosenId1);
-                    } else {
-                        coverOdd(chosenId1);
-                    }
-                    if (chosenId2 % 2 === 0) {
-                        coverEven(chosenId2);
-                    } else {
-                        coverOdd(chosenId2);
-                    }
-            }
-            
-            resetRound() //removes user input per round
-
-        } 
-    }); //closes click
-
-
-}
 
 
--- assets/js/oaf.js
+++ assets/js/oaf.js
@@ -1,60 +1,88 @@
 // -------------------------- Objects, Arrays, and Functions ----------------------
-
+//-------------------------set arrays
 let words = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"];
 
+// --------------------   global variables for functions
 var userInput = null;
 var roundTimer = 500;
 var viewClock;
 var mode; 
 var difficulty = [[easy],[medium],[hard]]; 
-var numberCards = [5,10,15];
 var language; 
 var timeOnClock;
 var cardCount;
 
 
-function flipEven(userInput) {      //flips even cards face up
-    $(".card-Id-"+userInput).parent().parent() 
-    .slideToggle(200, function() {
-        $(this).addClass("even").addClass("clicked").removeClass("card-img-even").addClass("faceUp").children().children().removeClass("invisible")
-    })
-    .slideToggle(200);
-}
-
-function flipOdd(userInput) {       //flips odd cards face up
-    $(".card-Id-"+userInput).parent().parent() 
-    .slideToggle(200, function() {
-        $(this).addClass("odd").addClass("clicked").removeClass("card-img-odd").addClass("faceUp").children().children().removeClass("invisible")
-    })
-    .slideToggle(200);
-}
-
-function coverEven(userInput) {     //flips even cards face down
-    setTimeout(function() {
-        $(".card-Id-"+userInput).parent().parent() 
-        .slideToggle(500, function() {
-            $(this).removeClass("even").removeClass("clicked").addClass("card-img-even").removeClass("faceUp").children().children().addClass("invisible")
+function flipEven(idNumber) {      //flips even cards face up
+    $(".card-Id-"+idNumber).parent().parent() 
+        .slideToggle(200, function() {
+            $(this)
+                .addClass("even")
+                .addClass("clicked")
+                .removeClass("card-img-even")
+                .addClass("faceUp")
+                .children()
+                .children()
+                .removeClass("invisible")
         })
+    .slideToggle(200);
+}
+
+function flipOdd(idNumber) {       //flips odd cards face up
+    $(".card-Id-"+idNumber).parent().parent() 
+        .slideToggle(200, function() {
+            $(this)
+                .addClass("odd")
+                .addClass("clicked")
+                .removeClass("card-img-odd")
+                .addClass("faceUp")
+                .children()
+                .children()
+                .removeClass("invisible")
+        })
+    .slideToggle(200);
+}
+
+function coverEven(idNumber) {     //flips even cards face down
+    setTimeout(function() {
+        $(".card-Id-"+idNumber).parent().parent() 
+            .slideToggle(500, function() {
+                $(this)
+                    .removeClass("even")
+                    .removeClass("clicked")
+                    .addClass("card-img-even")
+                    .removeClass("faceUp")
+                    .children()
+                    .children()
+                    .addClass("invisible")
+            })
         .slideToggle(500);
     },750)
 }
 
-function coverOdd(userInput) {  //flips odd cards face down
+function coverOdd(idNumber) {  //flips odd cards face down
     setTimeout(function() {
-        $(".card-Id-"+userInput).parent().parent() 
-        .slideToggle(500, function() {
-            $(this).removeClass("odd").removeClass("clicked").addClass("card-img-odd").removeClass("faceUp").children().children().addClass("invisible")
-        })
+        $(".card-Id-"+idNumber).parent().parent() 
+            .slideToggle(500, function() {
+                $(this)
+                    .removeClass("odd")
+                    .removeClass("clicked")
+                    .addClass("card-img-odd")
+                    .removeClass("faceUp")
+                    .children()
+                    .children()
+                    .addClass("invisible")
+            })
         .slideToggle(500);
     },750)
 
 }
 
-function matchedPair (userInput){   //fades out matched cards
-    $(".card-Id-"+userInput).parent().parent()
-    .animate({
-        opacity: 0
-    }, 200)
+function matchedPair (idNumber){   //fades out matched cards
+    $(".card-Id-"+idNumber).parent().parent()
+        .animate({
+            opacity: 0
+        }, 200)
     .addClass("card-matched");
 }
 
@@ -75,9 +103,15 @@ function timerPerRound (userInput) { //Round timer
         minutes = Math.floor(roundTimer / 60000)
         
         if (roundTimer <= -1 ){  //timer done break
-            return $(".timer-frame").children().first().text("Your Time is up!!")
+            return $(".timer-frame")
+                .children()
+                .first()
+                .text("Your Time is up!!");
         } else {
-            $(".timer-frame").children().first().text("You have " + minutes + " minutes and " + seconds + " seconds remaining")
+            $(".timer-frame")
+                .children()
+                .first()
+                .text("You have " + minutes + " minutes and " + seconds + " seconds remaining")
         }
         if (playerPoints === cardCount) {
             return;
@@ -125,10 +159,21 @@ function startScreen (mode= 0, difficulty = 0, numberCards=0, language=0, timeOn
     .append("<select id='timeOnClock'><option>1 minute</option><option>2 minutes</option><option>3 minutes</option><option>4 minutes</option><option>5 minutes</option></select>")//select
     $(".start-screen").children().filter("p").addClass("start-screen-description")
     $(".start-screen").children().filter("select").addClass("start-screen-choice")
-    $(".start-screen").append("<button class='start-button'>Start Game</button>")
+    $(".start-screen").append("<button class='start-button'>Start Game</button>");
     
-    
-    ;
+    $(".start-button").click(function() {  
+    // {[0]mode, [1]difficulty, [2]#cards, [3]language, [4]time }
+        $(".start-screen option:selected").each(function() {
+            playerModeSelection.push($(this).index());
+            $(".start-button").append(playerModeSelection);
+        });
+
+            cardPopulate();
+            playGame();
+    });
+
+
+
 }
 
 //----;---------------------------populate screen with cards------------------------------------
@@ -137,32 +182,128 @@ function cardPopulate ( timeOnClock = 60000) {  //start creation ready for card 
     var cardUniqueLabel = 0;    //creates unique labels for cards
 
     cardCount = (playerModeSelection[2] +1) *5;
-    console.log(playerModeSelection[2]);
+//    console.log(playerModeSelection[2]);
     maxPoints = cardCount;
 
-    $("#game-board").append("<div class='col-3 col-md-2 card-frame'><div class='row no-gutters'><div class='col-12'><span></span></div></div></div>");
-    $("#game-board").children().children().children().addClass("card-text-" + cardUniqueLabel).addClass("card-Id-"+cardUniqueLabel).addClass("card-word").addClass("invisible").text(words[cardUniqueLabel]);       //labels start div card-text-* 
-    $("#game-board").children().addClass("ml-3");                                                                          //labels start div for space (margin)
+    $(".start-screen")
+        .css("display", "none")
+        .css("z-index", "0");
 
-    for (cardUniqueLabel = 1; cardUniqueLabel < cardCount; cardUniqueLabel++ ) {                                         //labels divs for remaining card-text-*
+    $("#game-board")    //------------   Creates initial 0 index 
+        .append("<div class='col-3 col-md-2 card-frame'><div class='row no-gutters'><div class='col-12'><span></span></div></div></div>")
+        .children()
+        .addClass("ml-3")
+        .children()
+        .children()
+        .addClass("card-text-" + cardUniqueLabel)
+        .addClass("card-Id-"+cardUniqueLabel)
+        .addClass("card-word")
+        .addClass("invisible")
+        .text(words[cardUniqueLabel]);       //labels start div card-text-* 
+
+    for (cardUniqueLabel = 1; cardUniqueLabel < cardCount; cardUniqueLabel++ ) { //labels divs for  first half of cards card-text-*
         $("#game-board").append("<div class='col-3 col-md-2 card-frame'><div class='row no-gutters'><div class='col-12'><span></span></div></div></div>");
-        $("div[class*='card-text-']").last().parent().parent().next("div").children().children().addClass("card-text-" + cardUniqueLabel).addClass("card-Id-"+cardUniqueLabel).addClass("card-word").addClass("invisible").text(words[cardUniqueLabel]);  //adds card specific targets and text spacing
-        $("div[class*='card-text-']").last().parent().parent().addClass("ml-3");                                        //spaces (margins) cards
+        $("div[class*='card-text-']")
+            .last()
+            .parent()
+            .parent()
+            .next("div")
+            .addClass("ml-3")           //spaces (margins) cards
+            .children()
+            .children()
+            .addClass("card-text-" + cardUniqueLabel)
+            .addClass("card-Id-"+cardUniqueLabel)
+            .addClass("card-word")
+            .addClass("invisible")
+            .text(words[cardUniqueLabel]);  //adds card specific targets and text spacing                             
     }
 
      for (cardUniqueLabel = cardCount; cardUniqueLabel < cardCount + cardCount; cardUniqueLabel++ ) {                   //creates pair for matching
         $("#game-board").append("<div class='col-3 col-md-2 card-frame'><div class='row no-gutters'><div class='col-12'><span></span></div></div></div>");
-        $("div[class*='card-text-']").last().parent().parent().next("div").children().children().addClass("card-text-" + (cardUniqueLabel - cardCount)).addClass("card-Id-"+cardUniqueLabel).addClass("card-word").addClass("invisible").text(words[cardUniqueLabel - cardCount]);  //adds card specific targets and text spacing
-        $("div[class*='card-text-']").last().parent().parent().addClass("ml-3");                                        //spaces (margins) cards
-     }
+        $("div[class*='card-text-']")
+        .last()
+        .parent()
+        .parent()
+        .next("div")
+        .addClass("ml-3")                 //spaces (margins) cards
+        .children()
+        .children()
+        .addClass("card-text-" + (cardUniqueLabel - cardCount))
+        .addClass("card-Id-"+cardUniqueLabel)
+        .addClass("card-word")
+        .addClass("invisible")
+        .text(words[cardUniqueLabel - cardCount]);  //adds card specific targets and text spacing                            
+    }
     
     $(".card-frame").odd().addClass("card-img-odd");                                                                    //adds background to odd cards
     $(".card-frame").even().addClass("card-img-even");                                                                  //adds background to even cards
 
     timerPerRound (timeOnClock); //calls timer and sets time
+}
 
-    startScreen();
-    }
+
+
+
+
+function playGame() {
+
+    $(document).on("click", ".card-frame", function() {
+        if(gameMatchingPair === 0 && !$(this).hasClass("card-matched")) {
+            chosenIndex1 = $(this).index(); // catches index where click is
+            chosenId1 = chosenIndex1;       
+            gameMatchingPair++;
+        } else if (gameMatchingPair === 1 && chosenId1 !== $(this).index() && !$(this).hasClass("card-matched")) { // prevents same selections
+            chosenIndex2 = $(this).index();
+            chosenId2 = chosenIndex2;
+            gameMatchingPair++;
+        }
+
+        console.log("click"); // Error testing and console readability  
+
+        if ($(this).hasClass("card-img-even") && (!$(this).hasClass("clicked"))) {
+            flipEven ($(this).index());
+        } else if ($(this).hasClass("card-img-odd") && !$(this).hasClass("clicked")) {
+            flipOdd ($(this).index());
+        }
+
+        if (gameMatchingPair === 2) {
+            if(chosenIndex1 >= cardCount){     //makes chosenIndex's comparable for matching
+                chosenIndex1 = chosenIndex1 - cardCount;
+            } else if (chosenIndex2 >= cardCount) {
+                chosenIndex2 = chosenIndex2 -cardCount;
+            }
+
+            if(chosenIndex1 === chosenIndex2) {     //removes correct cards and reflips wrong
+                timeDelay(chosenId1);
+                timeDelay(chosenId2);
+                playerScored();
+            } else {
+
+                    if (chosenId1 % 2 === 0) {
+                        coverEven(chosenId1);
+                    } else {
+                        coverOdd(chosenId1);
+                    }
+                    if (chosenId2 % 2 === 0) {
+                        coverEven(chosenId2);
+                    } else {
+                        coverOdd(chosenId2);
+                    }
+            }
+            
+            resetRound() //removes user input per round
+
+        } 
+    }); //closes click
+
+
+}
+
+
+
+
+
+
 
 var cardFrame = {
 
