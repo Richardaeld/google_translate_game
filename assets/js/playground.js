ff4da49eca6b67914f745c2f4f2967b81ca79579
Richardaeld <richardaeld@gmail.com>
2020-09-08 02:09:56 +0000

    cleaned up and compressed existing code, changed element structure of cards to a more effecient style, and added a logic to 'flip' cards

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -1,62 +1,67 @@
 let words = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"];
-
-function oddEvenFace(indexNumber, target){
-    if (indexNumber % 2 === 0 ){
-        target.classList.add("card-img-even");
-    } else{
-        target.classList.add("card-img-odd");
-    }
-}
+var clickRecord = [];
 
 function insertWord(index){
-    var loc2 = document.getElementsByClassName("invisible")[i];
-    para = document.createElement("p");
-    node1 = document.createTextNode(words[i]);
+    var loc2 = document.getElementsByClassName("cardBack")[index];
+     para = document.createElement("p");
+     node1 = document.createTextNode(words[index]);
     para.appendChild(node1);
     loc2.appendChild(para);
 }
 
-function makeInnerLayers(cardNumber, className, classValues, classTarget = null){
-    for (i=0; i<cardNumber; i++){  
-        var loc1 = document.getElementsByClassName(className)[i];                       //creates parent target
-        var class1 = document.createAttribute("class");                                 //creates a empty variable to place
-        class1.value = classValues;                                                     //adds value to empty class
-        loc1.appendChild(document.createElement("div")).setAttributeNode(class1);       //creates div with class
-        
-        if(classTarget !== null){
-            insertWord(i)
+function constructCard (cardNumber, classValues, className=null, matchingPair = 0, isCardFace = false, isCardBack = false){
+    for (index = 0; index < cardNumber*2; index++){
+
+        var CardContainer = document.getElementById("game");
+        var cardInternal = document.getElementsByClassName(className)[index];
+        var setClasses = document.createAttribute("class");
+        setClasses.value = classValues;
+
+        if (className === null){ //container
+            CardContainer.appendChild(document.createElement("div")).setAttributeNode(setClasses);
+            continue;
+        }
+
+        if (index<cardNumber && className !== null){    //cardRotate, cardFace, cardBack
+            cardInternal.appendChild(document.createElement("div")).setAttributeNode(setClasses)
+            
+            if (isCardFace){
+                var identityTarget = document.getElementsByClassName("cardFace")[index];
+                identityTarget.id = "card-Id-" + index; 
+            }
+
+            if (isCardBack){
+                insertWord(index);
+            }
+
         }
     }
 }
 
 
+constructCard(5, "col-3 col-md-2 card-frame ml-3");  //container
+constructCard(10, "row no-gutters middle cardRotate", "col-3"); //cardRotate
+constructCard(10, "col-12 card cardFace", "middle", 1, true);     //cardFace
+constructCard(10, "col-12 card cardBack", "middle", 1, false, true);     //cardBack
 
-
-function makeCardsOuter(cardNumber, matchingPair = 0){
-
-    for (i=0; i<cardNumber + cardNumber; i++){  
-        var loc1 = document.getElementById("game");                                     //creates parent target
-        var class1 = document.createAttribute("class");                                 //creates a empty variable to place
-        class1.value = "col-3 col-md-2 card-frame ml-3";                                //adds value to empty class
-        loc1.appendChild(document.createElement("div")).setAttributeNode(class1);       //creates div with class
-        var loc2 = document.getElementById("game").getElementsByTagName("div")[i];      //creates location to add class to
-        loc2.id = "card-Id-" + i;                                                       //creates class to add 
-
-        if ( i >= cardNumber){                                                          //labels cards for matching pairs
-            loc2.className += " card-text-" + matchingPair;
-            matchingPair++
-        } else {
-            loc2.className += " card-text-" + i;
-        }
-        
-        oddEvenFace(i, loc2);
-    };        
+var cards = document.getElementsByClassName("cardFace").length;
+var loc6 = document.getElementsByClassName("cardFace");
+for(i=0; i<cards; i++){
+    loc6[i].onclick = function() {
+        this.parentElement.classList.add("flipCard");
+        clickRecord.push(this.id);
+    //    console.log(clickRecord);
+    }
 }
 
-makeCardsOuter(5);                                                      //makes outside frame for card
-makeInnerLayers(10, "col-3", "row no-gutters middle");                   //makes middle layer of card
-makeInnerLayers(10, "middle", "col-12 card-word invisible", 1);          //makes inner most layer of card
+function printme() {
+if (clickRecord.length === 2){
+    console.log(clickRecord);
+    clickRecord = [];
+}
+}
 
-
-
-
+document.onclick = function() {
+    console.log("You have the shot! Take the shot!!")
+   printme(); 
+} 
