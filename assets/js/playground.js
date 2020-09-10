3bc6acb50d965f8eabab28c39eb0f3aa18c7383b
Richardaeld <richardaeld@gmail.com>
2020-09-10 06:03:20 +0000

    added a random number generator to randomize card placement on game-board

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -2,7 +2,7 @@ let words = ["she", "look", "time", "could", "people", "part", "long", "did", "o
 var clickRecord = [];   //two cards player currently selected
 var playerPoints = 0;   //total matched pairs by player
 var MaxPlayerPoints = 0; // pair from start screen for wingame
-
+var rngArray = [];
 
 
 
@@ -21,17 +21,93 @@ function unflipWrongPair(clickRecord) {
         document.getElementById(clickRecord[1]).parentElement.classList.remove("flipCard");
     },2000);
 }
+//-------------------------------------------------------------------------
+    var rngList_0 = [];
+    var indexList_0 = [];
+    var final_0 = [];
+    var rngList_1 = [];
+    var indexList_1 = [];
+    var final_1 = [];    
 
+function createRNG(cardNumber, indexList, rngList){
+    for(i=0; i<cardNumber*2; i++){
+        if(indexList===0){
+            indexList_0.push(i);
+            console.log(indexList_0 + "     i give up");
+        } else {
+            indexList_1.push(i);
+        }
+    }
+    for(i=((cardNumber*2)); i>0; i-- ){
+        if (rngList===0){
+        rngList_0.push(Math.floor((Math.random()*i)));
+        } else{
+        rngList_1.push(Math.floor((Math.random()*i)));
+        }
+}
+
+ //   console.log(rngList +"AT GENERATOR");
+
+
+}
+
+
+//    for(i=0; i < 5; i++){ //i can be subbed with cardNumber
+//        indexList.push(i);
+//    }
+
+//    for(i=5; i > 0; i--){ //i can be subbed with cardNumber/
+//        var rng = Math.floor((Math.random()*i));
+//        rngList.push(rng);
+//    }
+
+
+
+//    for(i=0; i < 5; i++){
+//        final.push(indexList.splice((rngList[i]), 1));
+//    }
+
+//
+//        final.push(indexList.splice((rngList[i]), 1));
+//
+//console.log(indexList + " indexList");
+//console.log(rngList + " rngList");
+//console.log(final + " final");
+
+//-------------------------------------------------------------------------
 function makeCardFunctional(index, cardNumber, target){
         createParagraph = document.createElement("p");
-    if (index < cardNumber){ // makes a unique card id and a matching pair id and creates word for text node
-        target.firstChild.id = "cardId-" + index + "-" + index;
-        ParagraphNode = document.createTextNode(words[index]);
-    } else {
-        target.firstChild.id = "cardId-" + index + "-" + (index-cardNumber);
-        ParagraphNode = document.createTextNode(words[index-cardNumber]);
-    }
+        var rngIndex0;
+        var rngIndex1;
 
+       // rngIndex0 = final_0.push(indexList_0.splice((rngList_0[i]), 1));
+       // rngIndex1 = final_1.push(indexList_1.splice((rngList_1[i]), 1));
+
+    //   console.log(indexList_0 + " lskdjf;ljsadf");
+       console.log(rngList_0 + " lsssssssssssssssssf");
+    //   console.log(indexList_1 + " lskdjf;ljsadf111111111111");
+       console.log(rngList_1 + " lsssssssssssssssssf111111111111");       
+
+        final_0.push(indexList_0.splice((rngList_0[index]), 1));
+        final_1.push(indexList_1.splice((rngList_1[index]), 1));
+    //    console.log(indexList_0.splice((rngList_0[i]), 1)+ "  RNG generators are terrible");
+    //    console.log(indexList_1.splice((rngList_1[i]), 1)+ "  RNG generators are terrible");
+    //    console.log(final_0.push(indexList_0.splice((rngList_0[i]), 1)) + "this one!!");
+
+     //   console.log(final_0[index] + "  RNG generators are terrible");
+
+        rngIndex0=final_0[index];
+        rngIndex1=final_1[index];
+
+    //    console.log(rngIndex0 +  '     damnit!');
+    //    console.log(rngIndex1 + '      god');
+    if (rngIndex0 < cardNumber){ // makes a unique card id and a matching pair id and creates word for text node
+        target.firstChild.id = "cardId-" + index + "-" + rngIndex0;
+        ParagraphNode = document.createTextNode(words[rngIndex0]);
+    } else {
+        target.firstChild.id = "cardId-" + index + "-" + (rngIndex0-cardNumber);
+        ParagraphNode = document.createTextNode(words[rngIndex0-cardNumber]);
+    }
     createParagraph.appendChild(ParagraphNode);
     target.lastChild.appendChild(createParagraph);      //adds word to card
 
@@ -49,12 +125,10 @@ function constructCard (cardNumber, classValues, className=null, matchingPair = 
         var cardInternal = document.getElementsByClassName(className)[index];
         var setClasses = document.createAttribute("class");
         setClasses.value = classValues;
-        
         if (className === null){ //card container
             CardContainer.appendChild(document.createElement("div")).setAttributeNode(setClasses);
             continue;
         }
-
         cardInternal.appendChild(document.createElement("div")).setAttributeNode(setClasses) //adds elements for cardRotate cardFace and cardBack
         
         if (isCardBack){        //calls word adding function
@@ -76,7 +150,6 @@ function checkCardPair() {                //finds ID #
             console.log("You have the shot! Take the shot!!")
             removingCorrectPair(clickRecord);
             clickRecord = [];
-            
         } else {
             unflipWrongPair(clickRecord);
             clickRecord = [];
@@ -104,15 +177,18 @@ function startGame() {      //---------------------------------------collect use
 }
 
 function populateGame(mode, difficulty, numberOfCards, language, time ) {      //create game for play
+    createRNG(numberOfCards, 0, 0);//----------------------------------------------------------------------------------------------    
+    createRNG(numberOfCards, 1, 1);//----------------------------------------------------------------------------------------------
+
+    //    console.log(rngList_0 + "rngList_0");//---------------------------------------------
+    //    console.log(rngList_1 + "rngList_1");//----------------------------------------------
+
     constructCard(numberOfCards, "col-3 col-md-2 card-frame ml-3");  //container
     constructCard(numberOfCards, "row no-gutters middle cardRotate", "col-3"); //cardRotate
     constructCard(numberOfCards, "col-12 card cardFace", "middle", 1, true);     //cardFace
     constructCard(numberOfCards, "col-12 card cardBack", "middle", 1, false, true);     //cardBack
-
     MaxPlayerPoints = numberOfCards;
     gameHeader(2);
-
-    console.log(numberOfCards[2]);
     timer((time*60000)+1000); //change time into minutes and add 1 second so user sees full time minute value
 }
 
@@ -121,8 +197,6 @@ function timer(time) {
     setTimeout(function() {
         time -= 1000;
         setTimeElement.textContent = "Time remainging is " + Math.floor(time / 60000)  + " minute(s) and " + (time%60000)/1000 + " second(s)"; //changes time into a user display with minutes and seconds
-        console.log(time/1000);
-
         if(MaxPlayerPoints === playerPoints) { //win condition
             gameHeader(3)
             return;
@@ -178,3 +252,5 @@ function gameHeader (condition) { // 1-gamestartscreen ----- 2-gameplayscreen --
         restartGame();   
     }
 }
+
+
