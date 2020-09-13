c2904857b4e2d23d09a20617efc9802a7787d076
Richardaeld <richardaeld@gmail.com>
2020-09-13 19:16:32 +0000

    Added second language and new card background selection to start screen

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -1,10 +1,9 @@
-let words = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"];// original
-let words1 = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"]; //english
-let words2 = ["ella", "Mira", "hora", "podría", "personas", "parte", "larga/largo", "hizo", "en", "ellas/ellos", "yo", "estas/estos", "dijo", "entonces", "número", "No", "si"]; //spanish
-let words3 = ["ela", "Veja", "Tempo", "poderia", "pessoas", "parte", "longa/longo", "fez", "em", "eles", "Eu", "estes", "disse", "tão", "número", "não", "sim"]; //portuguese
-let words4 = ["elle", "Regardez", "temps", "pourrait", "gens", "partie", "longue/long", "fait", "sur", "elles/ils", "je", "celles-ci/ceux-ci", "m'a dit", "alors", "nombre", "non", "Oui"];//french
-let words5 = ["lei", "Guarda", "tempo", "poteva", "persone", "parte", "lunga/lungo", "fatta/fatto", "spora", "esse/essi", "io", "queste/questi", "disse", "così", "numero", "no", "sì"]; //italian
-let words6 = ["sie", "aussenhen", "Zeit", "könnten", "Menschen", "Teil", "lange", "tat", "auf", "Sie", "ich", "diese", "sagte", "so", "Nummer", "Nein", "Ja"]; //german
+let wordsEnglish = ["she", "look", "time", "could", "people", "part", "long", "did", "on", "they", "i", "these", "said", "so", "number", "no", "yes"]; //english
+let wordsSpanish = ["ella", "Mira", "hora", "podría", "personas", "parte", "larga/largo", "hizo", "en", "ellas/ellos", "yo", "estas/estos", "dijo", "entonces", "número", "No", "si"]; //spanish
+let wordsPortuguese = ["ela", "Veja", "Tempo", "poderia", "pessoas", "parte", "longa/longo", "fez", "em", "eles", "Eu", "estes", "disse", "tão", "número", "não", "sim"]; //portuguese
+let wordsFrench = ["elle", "Regardez", "temps", "pourrait", "gens", "partie", "longue/long", "fait", "sur", "elles/ils", "je", "celles-ci/ceux-ci", "m'a dit", "alors", "nombre", "non", "Oui"];//french
+let wordsItalian = ["lei", "Guarda", "tempo", "poteva", "persone", "parte", "lunga/lungo", "fatta/fatto", "spora", "esse/essi", "io", "queste/questi", "disse", "così", "numero", "no", "sì"]; //italian
+let wordsGerman = ["sie", "aussenhen", "Zeit", "könnten", "Menschen", "Teil", "lange", "tat", "auf", "Sie", "ich", "diese", "sagte", "so", "Nummer", "Nein", "Ja"]; //german
 
 function Language(english, spanish, portuguese, french, italian, german) {  //object constructor for foreign word calling
     this.english = english;
@@ -15,18 +14,28 @@ function Language(english, spanish, portuguese, french, italian, german) {  //ob
     this.german = german;
 }
 
+let cardFun = ["linear-gradient(to top left, #010203, #494949, #787878, #525252, #fff, #373737, #494949, #787878, #494949, #373737, #fff, #525252, #787878, #494949, #010203)", "#FF00EA" ]
+let cardIndex = ["linear-gradient(#fff 10%, #F015A7 10% 15%, #fff 15% 25%, #3172BB 25% 30%, #fff 30% 40%, #3172BB 40% 45%, #fff 45% 55%, #3172BB 55% 60%, #fff 60% 70%, #3172BB 70% 75%, #fff 75% 85%, #3172BB 85% 90%, #fff 90% 100%)", "#FF00EA" ]
+function CardStyle(index, fun) {
+    this.index = index;
+    this.fun = fun;
+}
+
 // ----- https://developer.mozilla.org/en-US/docs/Web/API/Document/height MDN height
 fullScreenHeight = document.documentElement.scrollHeight;       // makes background take up entire screen
 document.getElementById("playing-board").style.minHeight = fullScreenHeight + "px";
 
-let fLanguage = new Language(words1, words2, words3, words4, words5, words6);   //builds foreign language object for string calling
+let importCardStyle = new CardStyle (cardFun, cardIndex)
+let fLanguage = new Language(wordsEnglish, wordsSpanish, wordsPortuguese, wordsFrench, wordsItalian, wordsGerman);   //builds foreign language object for string calling
 var clickRecord = [];   //two cards player currently selected
 var rNG = [];  //first half is indexes that need to be used -- second half is randomized index list to pull first half with  
 var playerPoints = 0;   //total matched pairs by player
 var MaxPlayerPoints = 0; // pair from start screen for wingame
 var timeDelay = null; // for difficulty adjustment -- addes a click delay between card selection
 var globalDifficulty;   // user selection for difficulty
-var globalLanguage;     //user selection for foriegn langauge
+var globalLanguage0;     //user selection for langauge
+var globalLanguage1;     //user selection for langauge
+var globalCardType;
 
 function removingCorrectPair(clickRecord){  //removed correctly paired cards from gameboard
     setTimeout(function() {
@@ -59,10 +68,10 @@ function makeCardFunctional(index, cardNumber, target){     // gives cards funct
 
     if (rngIndex < cardNumber){ // makes a unique card id and a matching pair id and creates word for text node
         target.firstChild.id = "cardId-" + index + "-" + rngIndex;
-        ParagraphNode = document.createTextNode(words[rngIndex]);
+        ParagraphNode = document.createTextNode(fLanguage[globalLanguage0][rngIndex]);
     } else {
         target.firstChild.id = "cardId-" + index + "-" + (rngIndex-cardNumber);
-        ParagraphNode = document.createTextNode(fLanguage[globalLanguage][rngIndex-cardNumber]);
+        ParagraphNode = document.createTextNode(fLanguage[globalLanguage1][rngIndex-cardNumber]);
     }
 
     createParagraph.appendChild(ParagraphNode);
@@ -76,7 +85,7 @@ function makeCardFunctional(index, cardNumber, target){     // gives cards funct
     }
 }
 
-function constructCard (cardNumber, classValues, className=null, isCardBack = false){         //Creates the cards
+function constructCard (cardNumber, classValues, className = null, cardType = null, isCardBack = false ){         //Creates the cards
     for (index = 0; index < cardNumber*2; index++){
         var CardContainer = document.getElementById("game");
         var cardInternal = document.getElementsByClassName(className)[index];
@@ -89,7 +98,17 @@ function constructCard (cardNumber, classValues, className=null, isCardBack = fa
         }
 
         cardInternal.appendChild(document.createElement("div")).setAttributeNode(setClasses) //adds elements for cardRotate cardFace and cardBack
-        
+        if (cardType !== null){
+            cardInternal.lastChild.style.backgroundImage = importCardStyle[globalCardType][0];
+            cardInternal.lastChild.style.color = importCardStyle[globalCardType][1];
+        }
+
+
+//document.getElementById("playing-board").style.minHeight = fullScreenHeight + "px";
+
+
+
+
         if (isCardBack){        //calls word adding function
             makeCardFunctional(index, cardNumber, cardInternal);
         }
@@ -135,24 +154,26 @@ function startGame() {      //---------------------------------------collect use
     var userSelection = [];
     
     for(i=0; i<((startButton.length)); i++){ //gets user selection and parse into Int
-        if(startButton[i].selected && userSelection.length === 3){
+        if(startButton[i].selected && userSelection.length >= 3){
             userSelection.push(startButton[i].value);     //pull selected string
         } else if( startButton[i].selected) {
            userSelection.push(parseInt(startButton[i].value));     //pull selected number(int)
         }
     }
 
-    populateGame(userSelection[0],userSelection[1],userSelection[2],userSelection[3])
+    populateGame(userSelection[0],userSelection[1],userSelection[2],userSelection[3],userSelection[4], userSelection[5])
 }
 
-function populateGame(difficulty, numberOfCards, time, language ) {      //creates game for play by using user selections 
-    globalLanguage = language;
+function populateGame(difficulty, numberOfCards, time, language0, language1, cardType) {      //creates game for play by using user selections 
+    globalLanguage0 = language0;
+    globalLanguage1 = language1;
     globalDifficulty = difficulty;
+    globalCardType = cardType;
     createRNG(numberOfCards);//-------------------------------------RNG---------------------------------------------------------    
     constructCard(numberOfCards, "col-3 col-md-2 card-frame ml-3");  //container
     constructCard(numberOfCards, "row no-gutters middle cardRotate", "col-3"); //cardRotate
-    constructCard(numberOfCards, "col-12 card cardFace", "middle");     //cardFace
-    constructCard(numberOfCards, "col-12 card cardBack", "middle", true);     //cardBack
+    constructCard(numberOfCards, "col-12 card cardFace", "middle", cardType);     //cardFace
+    constructCard(numberOfCards, "col-12 card cardBack", "middle", cardType, true);     //cardBack
     MaxPlayerPoints = numberOfCards;
     gameHeader(2);
     timer((time*60000)+1000); //change time into minutes and add 1 second so user sees full time minute value
